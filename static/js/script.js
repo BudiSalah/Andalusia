59b7938ff75d1b46193b60495d8bb06d84b8cb27
BudiSalah <budi.salah@outlook.com>
2020-11-05 05:58:57 +0200

    finish header with responsive

---
+++ static/js/script.js
@@ -0,0 +1,56 @@
+window.addEventListener("DOMContentLoaded", main)
+
+function main() {
+    showDropDown();
+    splideSlider();
+}
+
+function showDropDown() {
+    let dropdowns = document.querySelectorAll(".dropdown");
+
+    if (dropdowns.length < 1) return;
+
+    for (dropdown of dropdowns) {
+        dropdown.addEventListener("mouseenter", toggleMenu)
+        dropdown.addEventListener("mouseleave", toggleMenu)
+
+        function toggleMenu(e) {
+            let target = e.target;
+            let dropBtn = target.querySelector(".dropdown-toggle");
+            let dropMenu = target.querySelector(".dropdown-menu");
+
+            setTimeout(() => {
+                if (!target.classList.contains("dropdown-menu") || !target.classList.contains("dropdown")) {
+                    if (e.type == "mouseenter") {
+                        target.classList.add("show")
+                        dropMenu.classList.add("show")
+                        dropBtn.setAttribute("aria-expanded", "true");
+                    } else {
+                        target.classList.remove("show")
+                        dropMenu.classList.remove("show")
+                        dropBtn.setAttribute("aria-expanded", "false");
+                    }
+                }
+            }, 200);
+        }
+    }
+}
+
+function splideSlider() {
+    let sliderItem = document.querySelector(".slider__item");
+    
+    if (sliderItem == null) return;
+
+    new Splide( '#splide', {
+        type: "loop",
+        direction: 'ttb',
+        height: `${sliderItem.clientHeight}px`,
+        // autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false
+    } ).mount();
+
+    let pagiWrapper = document.querySelector(".slider__index-list");
+    let splidePagi = document.querySelector(".splide__pagination");
+    pagiWrapper.insertAdjacentElement("afterbegin", splidePagi);
+}

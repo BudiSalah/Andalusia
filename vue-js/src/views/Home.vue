18d3deaf105b5ec131665eae3e6e5fb25108491a
BudiSalah <budi.salah@outlook.com>
2020-11-06 21:54:07 +0200

    finish overview section

--- vue-js/src/views/Home.vue
+++ vue-js/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Sliders />
     <Services />
+    <Overview />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import Sliders from "@/components/Sliders";
 import Services from "@/components/Services";
+import Overview from "@/components/Overview";
 
 export default {
   name: "Home",
   components: {
     Sliders,
     Services,
+    Overview
   },
 };
 </script>
